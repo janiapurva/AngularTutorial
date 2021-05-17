92b156c4279a9b957591a12cff505c0a70e9c624
janiapurva <jani.apurva@yahoo.com>
2021-05-17 18:19:30 -0400

    create emp_info services and display in browser

--- hello-world/src/app/emp-info/emp-info.component.ts
+++ hello-world/src/app/emp-info/emp-info.component.ts
@@ -1,13 +1,27 @@
+import { RecordsService } from './../records.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-emp-info',
   templateUrl: './emp-info.component.html',
-  styleUrls: ['./emp-info.component.css']
+  styleUrls: ['./emp-info.component.css'],
+  providers:[RecordsService]
 })
 export class EmpInfoComponent implements OnInit {
+  infoReceived1: string[]=[];
+  infoReceived2: string[]=[];
+  infoReceived3: string[]=[];
 
-  constructor() { }
+  getInfoFromServiceClass1 (){
+    this.infoReceived2 = this.rservice.getInfo1()
+  }
+  getInfoFromServiceClass2 (){
+    this.infoReceived2 = this.rservice.getInfo2()
+  }
+  getInfoFromServiceClass3 (){
+    this.infoReceived2 = this.rservice.getInfo3()
+  }
+
+  constructor(private rservice:RecordsService) { }
 
   ngOnInit(): void {
   }
--- hello-world/src/app/records.service.ts
+++ hello-world/src/app/records.service.ts
@@ -7,8 +7,14 @@ export class RecordsService {
   info1 = ['John Cena', 'M1HH3', 'john.cena@wwe.com'];
   info2 = ['The Rock', 'M9DDD', 'the.rock@wwe.com'];
   info3 = ['Great Nano', 'M1EEE', 'great.nano@wwe.com'];
-  getInfo(): string[] {
+  getInfo1(): string[] {
     return this.info1;
+  }
+  getInfo2(): string[] {
+    return this.info2;
+  }
+  getInfo3(): string[] {
+    return this.info3;
   }
   constructor() {}
 }
