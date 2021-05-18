8a1a41a32b2b671c31a491ecad0641c9b189256f
janiapurva <jani.apurva@yahoo.com>
2021-05-18 19:50:56 -0400

    learn Angular services

--- hello-world/src/app/data.service.ts
+++ hello-world/src/app/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  info1: string[] = ['John Mathew', 'E453', 'jm@abc.net'];
+  info2: string[] = ['Rob Nelson', 'E345', 'kd@fd.net'];
+  info3: string[] = ['Mathew Rode', 'R444', 'nd@fka.net'];
 
-  constructor() { }
+  getInfo1():string[]{
+    return this.info1
+  }
+  getInfo2():string[]{
+    return this.info2
+  }
+  getInfo3():string[]{
+    return this.info3
+  }
+
+  constructor() {}
 }
--- hello-world/src/app/e-info/e-info.component.ts
+++ hello-world/src/app/e-info/e-info.component.ts
@@ -1,23 +1,28 @@
+import { DataService } from './../data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-e-info',
   templateUrl: './e-info.component.html',
-  styleUrls: ['./e-info.component.css']
+  styleUrls: ['./e-info.component.css'],
+  providers: [DataService],
 })
 export class EInfoComponent implements OnInit {
- infoRecived1:string[] = [];
- infoRecived2:string[] = [];
- infoRecived3:string[] = [];
+  infoReceived1: string[] = [];
+  infoReceived2: string[] = [];
+  infoReceived3: string[] = [];
 
- getInfoService1(){}
- getInfoService2(){}
- getInfoService3(){}
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  getInfoService1() {
+    this.infoReceived1 = this.dservice.getInfo1();
+  }
+  getInfoService2() {
+    this.infoReceived2 = this.dservice.getInfo2();
+  }
+  getInfoService3() {
+    this.infoReceived3 = this.dservice.getInfo3();
   }
 
+  constructor(private dservice: DataService) {}
+
+  ngOnInit(): void {}
 }
